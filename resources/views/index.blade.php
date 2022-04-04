6d6ac91f41412dfd6de7f0654f241f3765b29205
serge <sergejzapivalovs@gmail.com>
2022-04-04 13:02:23 +0100

    index page, update on the hearder
    Signed-off-by: serge <sergejzapivalovs@gmail.com>

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,19 +4,19 @@
     <div class="background-image grid grid-cols-1 m-auto">
         <div class="flex text-gray-100 pt-10">
             <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block text-center">
-                <h1 class="sm:text-white text-5xl uppercase font-bold text-shadow-md pb-14">
-                    Do you want to become a developer?
+                <h1 class="sm:text-red-800 text-5xl font-bold text-shadow-md  pb-14">
+                    Movie.DKIT
                 </h1>
                 <a 
                     href="/blog"
-                    class="text-center bg-gray-50 text-gray-700 py-2 px-4 font-bold text-xl uppercase">
+                    class="text-center  text-red-800  hover:underline     font-bold text-xl uppercase">
                     Read More
                 </a>
             </div>
         </div>
     </div>
 
-    <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
+    <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-blie-200">
         <div>
             <img src="https://cdn.pixabay.com/photo/2014/05/03/01/03/laptop-336704_960_720.jpg" width="700" alt="">
         </div>
